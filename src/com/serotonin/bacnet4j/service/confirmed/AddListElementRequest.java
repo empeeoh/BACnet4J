3e4a0aac674243d4b3b99dd84e70a022894300ef
Matthew Lohbihler <ml@serotoninsoftware.com>
2008-04-25 03:56:48 -0400

    Implemented

--- src/com/serotonin/bacnet4j/service/confirmed/AddListElementRequest.java
+++ src/com/serotonin/bacnet4j/service/confirmed/AddListElementRequest.java
@@ -1,12 +1,21 @@
 package com.serotonin.bacnet4j.service.confirmed;
 
 import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.exception.BACnetErrorException;
 import com.serotonin.bacnet4j.exception.BACnetException;
-import com.serotonin.bacnet4j.exception.NotImplementedException;
+import com.serotonin.bacnet4j.exception.BACnetServiceException;
+import com.serotonin.bacnet4j.obj.BACnetObject;
+import com.serotonin.bacnet4j.obj.ObjectProperties;
+import com.serotonin.bacnet4j.obj.PropertyTypeDefinition;
 import com.serotonin.bacnet4j.service.acknowledgement.AcknowledgementService;
 import com.serotonin.bacnet4j.type.Encodable;
 import com.serotonin.bacnet4j.type.constructed.Address;
+import com.serotonin.bacnet4j.type.constructed.BACnetError;
+import com.serotonin.bacnet4j.type.constructed.SequenceOf;
+import com.serotonin.bacnet4j.type.enumerated.ErrorClass;
+import com.serotonin.bacnet4j.type.enumerated.ErrorCode;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.error.ChangeListError;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 import com.serotonin.util.queue.ByteQueue;
@@ -14,14 +23,14 @@ import com.serotonin.util.queue.ByteQueue;
 public class AddListElementRequest extends ConfirmedRequestService {
     public static final byte TYPE_ID = 8;
     
-    private final ObjectIdentifier eventObjectIdentifier;
+    private final ObjectIdentifier objectIdentifier;
     private final PropertyIdentifier propertyIdentifier;
     private final UnsignedInteger propertyArrayIndex;
-    private final Encodable listOfElements;
+    private final SequenceOf<? extends Encodable> listOfElements;
     
-    public AddListElementRequest(ObjectIdentifier eventObjectIdentifier, PropertyIdentifier propertyIdentifier, 
-            UnsignedInteger propertyArrayIndex, Encodable listOfElements) {
-        this.eventObjectIdentifier = eventObjectIdentifier;
+    public AddListElementRequest(ObjectIdentifier objectIdentifier, PropertyIdentifier propertyIdentifier, 
+            UnsignedInteger propertyArrayIndex, SequenceOf<? extends Encodable> listOfElements) {
+        this.objectIdentifier = objectIdentifier;
         this.propertyIdentifier = propertyIdentifier;
         this.propertyArrayIndex = propertyArrayIndex;
         this.listOfElements = listOfElements;
@@ -32,32 +41,61 @@ public class AddListElementRequest extends ConfirmedRequestService {
         return TYPE_ID;
     }
     
+    @SuppressWarnings("unchecked")
     @Override
     public AcknowledgementService handle(LocalDevice localDevice, Address from) throws BACnetException {
-        throw new NotImplementedException();
+        BACnetObject obj = localDevice.getObject(objectIdentifier);
+        if (obj == null)
+            throw createException(ErrorClass.property, ErrorCode.writeAccessDenied, new UnsignedInteger(1));
+        
+        Encodable e;
+        try {
+            e = obj.getProperty(propertyIdentifier, propertyArrayIndex);
+        }
+        catch (BACnetServiceException ex) {
+            throw createException(ErrorClass.property, ErrorCode.invalidArrayIndex, new UnsignedInteger(1));
+        }
+        if (!(e instanceof SequenceOf<?>))
+            throw createException(ErrorClass.property, ErrorCode.propertyIsNotAnArray, new UnsignedInteger(1));
+        
+        SequenceOf<Encodable> propList = (SequenceOf<Encodable>)e;
+        
+        for (Encodable pr : listOfElements)
+            propList.add(pr);
+        
+        return null;
+    }
+
+    private BACnetErrorException createException(ErrorClass errorClass, ErrorCode errorCode,
+            UnsignedInteger firstFailedElementNumber) {
+        return new BACnetErrorException(new ChangeListError(getChoiceId(),
+                new BACnetError(errorClass, errorCode), firstFailedElementNumber));
     }
 
     @Override
     public void write(ByteQueue queue) {
-        write(queue, eventObjectIdentifier, 0);
+        write(queue, objectIdentifier, 0);
         write(queue, propertyIdentifier, 1);
         writeOptional(queue, propertyArrayIndex, 2);
         writeEncodable(queue, listOfElements, 3);
     }
     
     AddListElementRequest(ByteQueue queue) throws BACnetException {
-        eventObjectIdentifier = read(queue, ObjectIdentifier.class, 0);
+        objectIdentifier = read(queue, ObjectIdentifier.class, 0);
         propertyIdentifier = read(queue, PropertyIdentifier.class, 1);
         propertyArrayIndex = readOptional(queue, UnsignedInteger.class, 2);
-        listOfElements = readEncodable(queue, eventObjectIdentifier.getObjectType(), propertyIdentifier,
-                propertyArrayIndex, 3);
+        PropertyTypeDefinition def = ObjectProperties.getPropertyTypeDefinition(
+                objectIdentifier.getObjectType(), propertyIdentifier);
+        listOfElements = readSequenceOf(queue, def.getClazz(), 3);
+//        listOfElements = readEncodable(queue, objectIdentifier.getObjectType(), propertyIdentifier,
+//                propertyArrayIndex, 3);
     }
 
     @Override
     public int hashCode() {
         final int PRIME = 31;
         int result = 1;
-        result = PRIME * result + ((eventObjectIdentifier == null) ? 0 : eventObjectIdentifier.hashCode());
+        result = PRIME * result + ((objectIdentifier == null) ? 0 : objectIdentifier.hashCode());
         result = PRIME * result + ((listOfElements == null) ? 0 : listOfElements.hashCode());
         result = PRIME * result + ((propertyArrayIndex == null) ? 0 : propertyArrayIndex.hashCode());
         result = PRIME * result + ((propertyIdentifier == null) ? 0 : propertyIdentifier.hashCode());
@@ -73,11 +111,11 @@ public class AddListElementRequest extends ConfirmedRequestService {
         if (getClass() != obj.getClass())
             return false;
         final AddListElementRequest other = (AddListElementRequest) obj;
-        if (eventObjectIdentifier == null) {
-            if (other.eventObjectIdentifier != null)
+        if (objectIdentifier == null) {
+            if (other.objectIdentifier != null)
                 return false;
         }
-        else if (!eventObjectIdentifier.equals(other.eventObjectIdentifier))
+        else if (!objectIdentifier.equals(other.objectIdentifier))
             return false;
         if (listOfElements == null) {
             if (other.listOfElements != null)
--- src/com/serotonin/bacnet4j/service/confirmed/RemoveListElementRequest.java
+++ src/com/serotonin/bacnet4j/service/confirmed/RemoveListElementRequest.java
@@ -1,13 +1,21 @@
 package com.serotonin.bacnet4j.service.confirmed;
 
 import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.exception.BACnetErrorException;
 import com.serotonin.bacnet4j.exception.BACnetException;
-import com.serotonin.bacnet4j.exception.NotImplementedException;
+import com.serotonin.bacnet4j.exception.BACnetServiceException;
+import com.serotonin.bacnet4j.obj.BACnetObject;
+import com.serotonin.bacnet4j.obj.ObjectProperties;
+import com.serotonin.bacnet4j.obj.PropertyTypeDefinition;
 import com.serotonin.bacnet4j.service.acknowledgement.AcknowledgementService;
+import com.serotonin.bacnet4j.type.Encodable;
 import com.serotonin.bacnet4j.type.constructed.Address;
-import com.serotonin.bacnet4j.type.constructed.ReadAccessSpecification;
+import com.serotonin.bacnet4j.type.constructed.BACnetError;
 import com.serotonin.bacnet4j.type.constructed.SequenceOf;
+import com.serotonin.bacnet4j.type.enumerated.ErrorClass;
+import com.serotonin.bacnet4j.type.enumerated.ErrorCode;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.error.ChangeListError;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 import com.serotonin.util.queue.ByteQueue;
@@ -15,14 +23,14 @@ import com.serotonin.util.queue.ByteQueue;
 public class RemoveListElementRequest extends ConfirmedRequestService {
     public static final byte TYPE_ID = 9;
     
-    private ObjectIdentifier eventObjectIdentifier;
-    private PropertyIdentifier propertyIdentifier;
-    private UnsignedInteger propertyArrayIndex;
-    private SequenceOf<ReadAccessSpecification> listOfElements;
+    private final ObjectIdentifier objectIdentifier;
+    private final PropertyIdentifier propertyIdentifier;
+    private final UnsignedInteger propertyArrayIndex;
+    private final SequenceOf<? extends Encodable> listOfElements;
     
-    public RemoveListElementRequest(ObjectIdentifier eventObjectIdentifier, PropertyIdentifier propertyIdentifier, 
-            UnsignedInteger propertyArrayIndex, SequenceOf<ReadAccessSpecification> listOfElements) {
-        this.eventObjectIdentifier = eventObjectIdentifier;
+    public RemoveListElementRequest(ObjectIdentifier objectIdentifier, PropertyIdentifier propertyIdentifier, 
+            UnsignedInteger propertyArrayIndex, SequenceOf<? extends Encodable> listOfElements) {
+        this.objectIdentifier = objectIdentifier;
         this.propertyIdentifier = propertyIdentifier;
         this.propertyArrayIndex = propertyArrayIndex;
         this.listOfElements = listOfElements;
@@ -35,29 +43,64 @@ public class RemoveListElementRequest extends ConfirmedRequestService {
     
     @Override
     public void write(ByteQueue queue) {
-        write(queue, eventObjectIdentifier, 0);
+        write(queue, objectIdentifier, 0);
         write(queue, propertyIdentifier, 1);
         writeOptional(queue, propertyArrayIndex, 2);
         write(queue, listOfElements, 3);
     }
     
     RemoveListElementRequest(ByteQueue queue) throws BACnetException {
-        eventObjectIdentifier = read(queue, ObjectIdentifier.class, 0);
+        objectIdentifier = read(queue, ObjectIdentifier.class, 0);
         propertyIdentifier = read(queue, PropertyIdentifier.class, 1);
         propertyArrayIndex = readOptional(queue, UnsignedInteger.class, 2);
-        listOfElements = readSequenceOf(queue, ReadAccessSpecification.class, 3);
+        PropertyTypeDefinition def = ObjectProperties.getPropertyTypeDefinition(
+                objectIdentifier.getObjectType(), propertyIdentifier);
+        listOfElements = readSequenceOf(queue, def.getClazz(), 3);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public AcknowledgementService handle(LocalDevice localDevice, Address from) throws BACnetException {
-        throw new NotImplementedException();
+        BACnetObject obj = localDevice.getObject(objectIdentifier);
+        if (obj == null)
+            throw createException(ErrorClass.property, ErrorCode.writeAccessDenied, new UnsignedInteger(1));
+        
+        Encodable e;
+        try {
+            e = obj.getProperty(propertyIdentifier, propertyArrayIndex);
+        }
+        catch (BACnetServiceException ex) {
+            throw createException(ErrorClass.property, ErrorCode.invalidArrayIndex, new UnsignedInteger(1));
+        }
+        if (!(e instanceof SequenceOf<?>))
+            throw createException(ErrorClass.property, ErrorCode.propertyIsNotAnArray, new UnsignedInteger(1));
+        
+        SequenceOf<Encodable> propList = (SequenceOf<Encodable>)e;
+        
+        for (int i=1; i<=propList.getCount(); i++) {
+            Encodable prop = propList.get(i);
+            if (prop != null) {
+                for (Encodable rem : listOfElements) {
+                    if (prop.equals(rem))
+                        propList.remove(i);
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    private BACnetErrorException createException(ErrorClass errorClass, ErrorCode errorCode,
+            UnsignedInteger firstFailedElementNumber) {
+        return new BACnetErrorException(new ChangeListError(getChoiceId(),
+                new BACnetError(errorClass, errorCode), firstFailedElementNumber));
     }
 
     @Override
     public int hashCode() {
         final int PRIME = 31;
         int result = 1;
-        result = PRIME * result + ((eventObjectIdentifier == null) ? 0 : eventObjectIdentifier.hashCode());
+        result = PRIME * result + ((objectIdentifier == null) ? 0 : objectIdentifier.hashCode());
         result = PRIME * result + ((listOfElements == null) ? 0 : listOfElements.hashCode());
         result = PRIME * result + ((propertyArrayIndex == null) ? 0 : propertyArrayIndex.hashCode());
         result = PRIME * result + ((propertyIdentifier == null) ? 0 : propertyIdentifier.hashCode());
@@ -73,11 +116,11 @@ public class RemoveListElementRequest extends ConfirmedRequestService {
         if (getClass() != obj.getClass())
             return false;
         final RemoveListElementRequest other = (RemoveListElementRequest) obj;
-        if (eventObjectIdentifier == null) {
-            if (other.eventObjectIdentifier != null)
+        if (objectIdentifier == null) {
+            if (other.objectIdentifier != null)
                 return false;
         }
-        else if (!eventObjectIdentifier.equals(other.eventObjectIdentifier))
+        else if (!objectIdentifier.equals(other.objectIdentifier))
             return false;
         if (listOfElements == null) {
             if (other.listOfElements != null)
