de9fb121aa8e66c3e1e367b8bc1f7f84e77233a4
Matthew Lohbihler <ml@serotoninsoftware.com>
2010-05-25 13:44:54 -0400

    Tests

--- src_test/com/serotonin/bacnet4j/test/SerializationTests.java
+++ src_test/com/serotonin/bacnet4j/test/SerializationTests.java
@@ -12,12 +12,15 @@ import java.util.GregorianCalendar;
 import com.serotonin.bacnet4j.type.Encodable;
 import com.serotonin.bacnet4j.type.constructed.BACnetError;
 import com.serotonin.bacnet4j.type.constructed.ServicesSupported;
+import com.serotonin.bacnet4j.type.constructed.StatusFlags;
 import com.serotonin.bacnet4j.type.constructed.TimeValue;
 import com.serotonin.bacnet4j.type.enumerated.ErrorClass;
 import com.serotonin.bacnet4j.type.enumerated.ErrorCode;
 import com.serotonin.bacnet4j.type.enumerated.ObjectType;
 import com.serotonin.bacnet4j.type.error.BaseError;
 import com.serotonin.bacnet4j.type.error.ChangeListError;
+import com.serotonin.bacnet4j.type.notificationParameters.CommandFailure;
+import com.serotonin.bacnet4j.type.notificationParameters.NotificationParameters;
 import com.serotonin.bacnet4j.type.primitive.BitString;
 import com.serotonin.bacnet4j.type.primitive.Boolean;
 import com.serotonin.bacnet4j.type.primitive.CharacterString;
@@ -52,6 +55,8 @@ public class SerializationTests {
         Encodable deserialized;
         if (BaseError.class.isAssignableFrom(encodable.getClass()))
             deserialized = BaseError.createBaseError(queue);
+        else if (NotificationParameters.class.isAssignableFrom(encodable.getClass()))
+            deserialized = NotificationParameters.createNotificationParameters(queue);
         else {
             Constructor<? extends Encodable> c = encodable.getClass().getConstructor(ByteQueue.class);
             deserialized = c.newInstance(queue);
@@ -80,10 +85,11 @@ public class SerializationTests {
             new Unsigned8(254), //
             new UnsignedInteger(new BigInteger(Long.toString(Long.MAX_VALUE))), //
             new ServicesSupported(), //
-            new ChangeListError((byte) 9, new BACnetError(ErrorClass.object, ErrorCode.abortBufferOverflow),
-                    new UnsignedInteger(13)), //            
 
             // Constructed
             new TimeValue(new Time(13, 23, 12, 45), new Real(65.56F)), //
+            new ChangeListError((byte) 9, new BACnetError(ErrorClass.object, ErrorCode.abortBufferOverflow),
+                    new UnsignedInteger(13)), //
+            new CommandFailure(new Real(123.45F), new StatusFlags(false, true, false, true), new Time()), //
     };
 }
