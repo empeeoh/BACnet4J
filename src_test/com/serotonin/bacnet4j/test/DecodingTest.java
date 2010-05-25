4e796163e34f02ca1e9bb05e57787a3304d53ad3
Matthew Lohbihler <ml@serotoninsoftware.com>
2010-05-25 12:57:32 -0400

    *** empty log message ***

---
+++ src_test/com/serotonin/bacnet4j/test/DecodingTest.java
@@ -0,0 +1,118 @@
+package com.serotonin.bacnet4j.test;
+
+import com.serotonin.bacnet4j.npdu.ip.IpMessageControl;
+
+public class DecodingTest {
+    public static void main(String[] args) throws Exception {
+        // String input = "[81,b,0,c,1,20,ff,ff,0,ff,10,8]";
+        // String input = "[81,b,0,14,1,0,10,0,c4,2,0,8,98,22,1,e0,91,0,21,8]";
+        // String input = "[81,4,0,23,c0,a8,1,5a,ba,c0,1,8,27,28,6,0,40,ae,0,73,8f,10,0,c4,2,0,9,60,22,1,e0,91,0,21,8]";
+        // String input = "[81,4,0,1e,c0,a8,1,5a,ba,c0,1,8,4e,38,1,3,10,0,c4,2,0,9,63,22,1,e0,91,0,21,8]";
+        // String input = "[81,4,0,1e,c0,a8,1,5a,ba,c0,1,8,4e,36,1,1,10,0,c4,2,0,8,99,22,1,e0,91,0,21,8]";
+        // String input = "[81,4,0,1e,c0,a8,1,5a,ba,c0,1,8,4e,38,1,4,10,0,c4,2,0,9,64,22,1,e0,91,0,21,8]";
+        // String input = "[81,4,0,1e,c0,a8,1,5a,ba,c0,1,8,4e,36,1,2,10,0,c4,2,0,8,9a,22,1,e0,91,0,21,8]";
+        // String input = "[81,b,0,1b,1,28,ff,ff,0,27,28,6,0,40,ae,0,73,8f,fe,10,8,a,9,62,1a,9,62]";
+        // String input = "[81,b,0,1b,1,28,ff,ff,0,27,28,6,0,40,ae,0,73,8f,fe,10,8,a,9,61,1a,9,61]";
+        // String input = "[81,b,0,12,1,88,27,28,6,0,40,ae,0,73,8f,0,9c,58]";
+        // String input =
+        // "[81,b,0,2b,1,0,10,4,9,8,19,1,2e,c,2,0,8,98,19,0,29,0,3e,c,0,80,0,2e,19,55,3e,44,42,c8,0,0,3f,5b,4,0,0,3f,2f]";
+        // String input =
+        // "[81,b,0,2b,1,0,10,4,9,8,19,1,2e,c,2,0,8,98,19,0,29,0,3e,c,0,80,0,2e,19,55,3e,44,42,c8,0,0,3f,5b,4,0,0,3f,2f]";
+
+        // String input = "81|0a|00|17|01|20|ff|ff |00|ff|10|07|3d|09|00|65|69 |62|74|65|6d|70|31";
+        // String input = "81 |0a|00|1d|01|00|10|01|c4|02|00|04 |40|c4|00|00|00|02|75|09|00|65|69 |62|74|65|6d|70|31";
+        // String input = "81|0a|00|1d|01|20|ff|ff |00|ff|10|07|0a|04|40|1a|04|40 |3d|09|00|65|69|62|74|65|6d |70|31";
+
+        // WhoIs sent by Delta equipment.
+        // String input = "81 0b 00 1b 01 28 ff ff 00 27 d8 06 00 18 71 73 0d 8a fe 10 08 0a 11 5e 1a c3 4f";
+
+        // IAm response
+        // String input = "81 0b 00 1b 01 28 ff ff 00 c4 80 01 04 fc 10 00 c4 02 00 76 c4 21 80 91 03 21 08";
+        // String input = "[81,b,0,18,1,20,ff,ff,0,ff,10,0,c4,2,1,2c,c8,22,5,c0,91,3,21,4d]";
+        // String input = "81,a,0,17,1,4,2,75,1,e,c,2,1,2c,c8,1e,9,4d,9,62,9,8b,1f";
+
+        // Delta private message.
+        // String input =
+        // "81 0b 00 4b 01 a8 ff ff 00 28 3c 06 00 40 ae 00 20 28 ff 81 00 08 30 75 00 00 07 00 ff ff 04 00 23 00 31 00 00 00 00 00 00 00 00 00 00 00 40 ae 00 00 02 04 00 91 01 93 01 46 00 00 00 00 00 0c 00 01 00 06 00 20 29 01 00 00 00";
+        // String input =
+        // "81 0b 00 4b 01 a8 ff ff 00 28 3c 06 00 40 ae 00 20 28 ff 81 00 08 30 75 00 00 07 00 ff ff 04 00 23 00 31 00 00 00 00 00 00 00 00 00 00 00 40 ae 00 00 02 04 00 00 00 00 00 1e 00 00 00 00 00 0c 00 01 00 06 00 20 29 01 00 00 00";
+        // String input =
+        // "81 0b 00 45 01 28 ff ff 00 c4 7d 01 04 fd 10 04 09 08 19 01 2e 0c 02 00 75 98 19 00 29 00 3e 0c 00 00 00 65 19 55 3e 44 41 b5 3a 7a 3f 5b 04 00 00 0c 00 80 00 01 19 55 3e 44 41 b4 00 00 3f 5b 04 00 00 3f 2f";
+        // String input = "81 0a 00 0d 01 00 50 04 09 91 02 91 2a";
+
+        String s = "810a049a010800140600000000000030030e"
+                + "0c058000001e294b4ec4058000004f294d4e7510004d79204e6577204f626a"
+                + "65637420304f294f4e91164f29554e91004f29a44e91004f291c4e71004f291f"
+                + "4e71004f296f4e8204004f29244e91004f29674e91004f29514e104f2a03ef4e"
+                + "104f29a04e91004f29af4e4f29a34e91004f29a14e91004f299e4e91004f29a5"
+                + "4e4f299f4e4f29714e21004f29114e21004f29a64e4f29074e4f29274e4f2923"
+                + "4e8205004f29004e8205e04f29484e91014f29824e2ea4ffffffffb4ffffffff2f2ea"
+                + "4ffffffffb4ffffffff2f2ea4ffffffffb4ffffffff2f4f29a84e7508003132332d4c535a4f2"
+                + "a03e94e104f2a03ee4e71004f1f0c020000001e294d4e7513004e657720566"
+                + "9727475616c204465766963654f291c4e71004f293a4e750a006c6f63616c68"
+                + "6f73744f290c4e7400312e304f290a4e220bb84f290b4e220bb84f29494e210"
+                + "14f294b4ec4020000004f294f4e91084f29704e91004f29794e750d00534341"
+                + "444120456e67696e654f29784e217b4f29464e752500534341444120456e67"
+                + "696e652053657276657220666f722057696e646f77732056312e304f292c4e"
+                + "750700322e302e32324f29624e21014f298b4e21044f29614e850600fffbb83f"
+                + "ff4f29604e850501ffffffde4f294c4ec402000000c4058000004f293e4e220400"
+                + "4f29a74e21014f296b4e91004f29394eb40b0803254f29384ea46e0518014f2"
+                + "9774e3201e04f29184e114f29744e4f291e4ec402000000211465060000000"
+                + "00000c4023fffff21016506a9fee422bac0c4023fffff211461004f299b4e21004f"
+                + "299a4e4f299d4e2ea4ffffffffb4ffffffff2f4f29994e213c4f29984e4f2a02004e11"
+                + "4f2a02084e7536005363616e6e696e672044657669636573203a204c6f7720"
+                + "4c696d6974203d20302048696768204c696d6974203d20343139343330324f"
+                + "2a02014e233fffff4f29a84e7508003132332d4445564f2a03ed4e4f294d4e75"
+                + "13004e6577205669727475616c204465766963654f291c4e71004f293a4e75"
+                + "0a006c6f63616c686f73744f290c4e7400312e304f290a4e220bb84f290b4e2"
+                + "20bb84f29494e21014f294b4ec4020000004f294f4e91084f29704e91004f297"
+                + "94e750d00534341444120456e67696e654f29784e217b4f29464e752500534"
+                + "341444120456e67696e652053657276657220666f722057696e646f7773205"
+                + "6312e304f292c4e750700322e302e32324f29624e21014f298b4e21044f2961"
+                + "4e850600fffbb83fff4f29604e850501ffffffde4f294c4ec402000000c40580000"
+                + "04f293e4e2204004f29a74e21014f296b4e91004f29394eb40b0803384f2938"
+                + "4ea46e0518014f29774e3201e04f29184e114f29744e4f291e4ec4020000002"
+                + "1146506000000000000c4023fffff21016506a9fee422bac0c4023fffff2114610"
+                + "04f299b4e21004f299a4e4f299d4e2ea4ffffffffb4ffffffff2f4f29994e213c4f299"
+                + "84e4f2a02004e114f2a02084e7536005363616e6e696e67204465766963657"
+                + "3203a204c6f77204c696d6974203d20302048696768204c696d6974203d203"
+                + "43139343330324f2a02014e233fffff4f29a84e7508003132332d4445564f2a0" + "3ed4e4f1f";
+
+        new IpMessageControl().testDecoding(toBytes(s));
+    }
+
+    // public static void main(String[] args) throws Exception {
+    // String s = "[1e,9,a,1e,1f,1f,4f,29,4e,4e,c,0,0,0,0,19,0,29,0,3c,0,0,0,0,4f,29,23,"
+    // + "4e,82,5,0,4f,29,0,4e,82,5,e0,4f,29,11,4e,21,0,4f,29,4b,4e,c4,2,40,0,0,4f,2"
+    // + "9,4f,4e,91,9,4f,29,24,4e,91,0,4f,29,82,4e,2e,a4,ff,ff,ff,ff,b4,ff,ff,ff,ff,2f,2e,"
+    // + "a4,ff,ff,ff,ff,b4,ff,ff,ff,ff,2f,2e,a4,ff,ff,ff,ff,b4,ff,ff,ff,ff,2f,4f,29,a8,4e,75,7,0,3"
+    // + "1,32,33,2d,45,45,4f,2a,3,e9,4e,10,4f,2a,3,ee,4e,71,0,4f,1f]";
+    // byte[] b = toBytes(s);
+    //
+    // NotificationParameters.createNotificationParameters(new ByteQueue(b));
+    // }
+
+    public static byte[] toBytes(String s) {
+        if (s.startsWith("["))
+            s = s.substring(1);
+        if (s.endsWith("]"))
+            s = s.substring(0, s.length() - 1);
+        String[] parts = s.split(",");
+        if (parts.length == 1)
+            parts = s.split("\\|");
+        if (parts.length == 1)
+            parts = s.split(" ");
+        if (parts.length == 1) {
+            parts = new String[s.length() / 2];
+            for (int i = 0; i < parts.length; i++)
+                parts[i] = s.substring(i * 2, i * 2 + 2);
+        }
+
+        byte[] bytes = new byte[parts.length];
+
+        for (int i = 0; i < bytes.length; i++)
+            bytes[i] = (byte) Integer.parseInt(parts[i].trim(), 16);
+
+        return bytes;
+    }
+}
