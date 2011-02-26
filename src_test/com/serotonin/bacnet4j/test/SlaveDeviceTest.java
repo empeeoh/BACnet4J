c56b52661b61280e1f092d9e85bee670ebfb63d7
Matthew Lohbihler <ml@serotoninsoftware.com>
2011-02-25 22:51:35 -0500

    Added event listener

--- src_test/com/serotonin/bacnet4j/test/SlaveDeviceTest.java
+++ src_test/com/serotonin/bacnet4j/test/SlaveDeviceTest.java
@@ -25,15 +25,31 @@ package com.serotonin.bacnet4j.test;
 import java.io.File;
 
 import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.RemoteDevice;
+import com.serotonin.bacnet4j.RemoteObject;
+import com.serotonin.bacnet4j.event.DeviceEventListener;
 import com.serotonin.bacnet4j.obj.BACnetObject;
 import com.serotonin.bacnet4j.obj.FileObject;
+import com.serotonin.bacnet4j.service.confirmed.ReinitializeDeviceRequest.ReinitializedStateOfDevice;
+import com.serotonin.bacnet4j.type.Encodable;
+import com.serotonin.bacnet4j.type.constructed.Choice;
+import com.serotonin.bacnet4j.type.constructed.DateTime;
+import com.serotonin.bacnet4j.type.constructed.PropertyValue;
+import com.serotonin.bacnet4j.type.constructed.SequenceOf;
+import com.serotonin.bacnet4j.type.constructed.TimeStamp;
 import com.serotonin.bacnet4j.type.enumerated.BinaryPV;
 import com.serotonin.bacnet4j.type.enumerated.EngineeringUnits;
+import com.serotonin.bacnet4j.type.enumerated.EventState;
+import com.serotonin.bacnet4j.type.enumerated.EventType;
 import com.serotonin.bacnet4j.type.enumerated.FileAccessMethod;
+import com.serotonin.bacnet4j.type.enumerated.MessagePriority;
+import com.serotonin.bacnet4j.type.enumerated.NotifyType;
 import com.serotonin.bacnet4j.type.enumerated.ObjectType;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.notificationParameters.NotificationParameters;
 import com.serotonin.bacnet4j.type.primitive.Boolean;
 import com.serotonin.bacnet4j.type.primitive.CharacterString;
+import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.Real;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 
@@ -43,6 +59,7 @@ public class SlaveDeviceTest {
         localDevice.getConfiguration().setProperty(PropertyIdentifier.objectName,
                 new CharacterString("BACnet4J slave device test"));
         localDevice.setPort(2068);
+        localDevice.getEventHandler().addListener(new Listener());
         // localDevice.getConfiguration().setProperty(PropertyIdentifier.segmentationSupported,
         // Segmentation.noSegmentation);
 
@@ -135,6 +152,70 @@ public class SlaveDeviceTest {
             bi1.setProperty(PropertyIdentifier.presentValue, bi1value ? BinaryPV.active : BinaryPV.inactive);
 
             Thread.sleep(5000);
+        }
+    }
+
+    static class Listener implements DeviceEventListener {
+        @Override
+        public void listenerException(Throwable e) {
+            // no op
+        }
+
+        @Override
+        public void iAmReceived(RemoteDevice d) {
+            // no op
+        }
+
+        @Override
+        public boolean allowPropertyWrite(BACnetObject obj, PropertyValue pv) {
+            return true;
+        }
+
+        @Override
+        public void propertyWritten(BACnetObject obj, PropertyValue pv) {
+            System.out.println("Wrote " + pv + " to " + obj);
+        }
+
+        @Override
+        public void iHaveReceived(RemoteDevice d, RemoteObject o) {
+            // no op
+        }
+
+        @Override
+        public void covNotificationReceived(UnsignedInteger subscriberProcessIdentifier, RemoteDevice initiatingDevice,
+                ObjectIdentifier monitoredObjectIdentifier, UnsignedInteger timeRemaining,
+                SequenceOf<PropertyValue> listOfValues) {
+            // no op
+        }
+
+        @Override
+        public void eventNotificationReceived(UnsignedInteger processIdentifier, RemoteDevice initiatingDevice,
+                ObjectIdentifier eventObjectIdentifier, TimeStamp timeStamp, UnsignedInteger notificationClass,
+                UnsignedInteger priority, EventType eventType, CharacterString messageText, NotifyType notifyType,
+                Boolean ackRequired, EventState fromState, EventState toState, NotificationParameters eventValues) {
+            // no op
+        }
+
+        @Override
+        public void textMessageReceived(RemoteDevice textMessageSourceDevice, Choice messageClass,
+                MessagePriority messagePriority, CharacterString message) {
+            // no op
+        }
+
+        @Override
+        public void privateTransferReceived(UnsignedInteger vendorId, UnsignedInteger serviceNumber,
+                Encodable serviceParameters) {
+            // no op
+        }
+
+        @Override
+        public void reinitializeDevice(ReinitializedStateOfDevice reinitializedStateOfDevice) {
+            // no op
+        }
+
+        @Override
+        public void synchronizeTime(DateTime dateTime, boolean utc) {
+            // no op
         }
     }
 }
