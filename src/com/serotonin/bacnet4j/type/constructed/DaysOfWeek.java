46580a40d33734fd858629266281cac060072e79
Matthew Lohbihler <ml@serotoninsoftware.com>
2011-03-31 12:34:41 -0400

    License update

--- src/com/serotonin/bacnet4j/type/constructed/DaysOfWeek.java
+++ src/com/serotonin/bacnet4j/type/constructed/DaysOfWeek.java
@@ -17,6 +17,11 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * When signing a commercial license with Serotonin Software Technologies Inc.,
+ * the following extension to GPL is made. A special exception to the GPL is 
+ * included to allow you to distribute a combined work that includes BAcnet4J 
+ * without being obliged to provide the source code for any proprietary components.
  */
 package com.serotonin.bacnet4j.type.constructed;
 
